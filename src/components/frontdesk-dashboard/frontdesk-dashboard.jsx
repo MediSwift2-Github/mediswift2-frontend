a3e4376669b2a5ff293f38dfb80cf80cd23f7599
Rushabh <rushabghoghari@gmail.com>
2024-04-04 21:12:14 +0530

    WIP Fix table

--- src/components/frontdesk-dashboard/frontdesk-dashboard.jsx
+++ src/components/frontdesk-dashboard/frontdesk-dashboard.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import { Box, Typography, Toolbar } from '@mui/material';
+import React from "react";
+import { Box, Typography, Toolbar } from "@mui/material";
 import { AppBarComponent } from "./AppBarComponent/app-bar"; // Adjust the import path as necessary
 import { DrawerComponent } from "./drawer-component/drawer"; // Adjust the import path as necessary
 import { PatientTable } from "./patient-queue-table/patient-table"; // Adjust the import path as necessary
 
 const FrontDeskDashboard = () => {
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <AppBarComponent />
-            <DrawerComponent />
-            <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
-                <Toolbar />
-                <Typography variant="h6" gutterBottom>
-                    Patient List
-                </Typography>
-                <PatientTable />
-                {/* The PatientDialog components are removed from here since they are now in DrawerComponent */}
-            </Box>
-        </Box>
-    );
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBarComponent />
+      <DrawerComponent />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          bgcolor: "background.default",
+          p: 3,
+          height: 500,
+          marginTop: 10,
+        }}
+      >
+        <Toolbar />
+        <Typography variant="h6" gutterBottom>
+          Patient List
+        </Typography>
+        <PatientTable />
+        {/* The PatientDialog components are removed from here since they are now in DrawerComponent */}
+      </Box>
+    </Box>
+  );
 };
 
 export default FrontDeskDashboard;
